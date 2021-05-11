226878353e146891f5c98164f7700a24a0750ce0
Narath Carlile <narathc@gmail.com>
2021-05-11 09:47:22 -0400

    Minor update to active projects

--- move_to_active_projects.groovy
+++ move_to_active_projects.groovy
@@ -14,6 +14,12 @@ if (!nodeNext) {
 def moveToProjects = c.find{ it.text == 'Projects!' }[0]
 def moveTo = c.find{ it.text == 'Active Projects!' }[0]
 
+// We used to keep all projects in Projects, and clone into active projects
+// this unfortunately causes issues when quickly searching for projects (since it appears in 2 places)
+// most of the time, the place I would like the search to find it in is in Active Project
+// This would be a minor inconvenience except that deleting a cloned project does not delete the clones (it just leaves it there)
+// which is okay for me to realize, since it means that I can freely delete it from active projects without bothering to move it back
+// so I will leave them as clones there, so 
 // in general we expect active project to come from projects
 // if in projects, then clone into Active Projects
 // if in active projects, then clone into active Projects (elevating a subtask)
